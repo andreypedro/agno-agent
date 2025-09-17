a84e52bbace455a4d0dc4b11c40efd9dcf7e4b8c
Andrey Lefkum <andreypedro@gmail.com>
2025-09-17 13:22:55 -0400

    feat: add agno os

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import os
-from dotenv import load_dotenv
 from agno.agent import Agent
+from agno.db.sqlite import SqliteDb
 from agno.models.openrouter import OpenRouter
-from agno.tools.hackernews import HackerNewsTools
+from agno.os import AgentOS
+from agno.tools.mcp import MCPTools
 
-load_dotenv()
-
-agent = Agent(
+agno_agent = Agent(
+    name="Agno Agent",
     model=OpenRouter(
                 id="meituan/longcat-flash-chat",
                 api_key=os.getenv('OPENROUTER_API_KEY')
             ),
-   #  tools=[HackerNewsTools()],
+    
+    # Add a database to the Agent
+    db=SqliteDb(db_file="agno.db"),
+    
+    # Add the Agno MCP server to the Agent
+    tools=[MCPTools(transport="streamable-http", url="https://docs.agno.com/mcp")],
+    
+    # Add the previous session history to the context
+    add_history_to_context=True,
     markdown=True,
 )
-# agent.print_response("Summarize the top 5 stories on hackernews", stream=True)
-agent.print_response("Share a 2 sentence horror story.")
+
+
+# Create the AgentOS
+agent_os = AgentOS(agents=[agno_agent])
+
+# Get the FastAPI app for the AgentOS
+app = agent_os.get_app()
